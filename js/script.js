8b4b868c8a2fac22eaff382be712a7f106e2ad06
NoName43221 <slavikvlasenko777@gmail.com>
2021-11-22 22:14:40 +0300

    ekek

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(".content1").hide(1);
+$(".content1").show(4000);
+
+$(window).on('scroll', (e) => {
+    if (window.scrollY < 150) {
+        $( ".content2, .footer" ).hide( {
+        duration: 2000, // продолжительность анимации
+        easing: "linear" // скорость анимации
+        });
+    } 
+    else {
+        $(".content2, .footer").show( {
+            duration: 4000, // продолжительность анимации
+            easing: "swing" // скорость анимации
+        });
+    }
+});
+
+ 
