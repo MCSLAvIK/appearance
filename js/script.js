af12a184a6c1f0016a2e7b6782aa504fbd82f8b8
NoName43221 <slavikvlasenko777@gmail.com>
2021-11-22 22:22:34 +0300

    T

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(".content1").show(4000);
 $(window).on('scroll', (e) => {
     if (window.scrollY < 50) {
         $( ".content2, .footer" ).hide( {
-        duration: 2000, // продолжительность анимации
+        duration: 1, // продолжительность анимации
         easing: "linear" // скорость анимации
         });
     } 
